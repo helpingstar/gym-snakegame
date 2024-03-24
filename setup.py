d9d5bd61daa95ebb4b9dcb67e459e77e38ffd50e
helpingstar <iamhelpingstar@gmail.com>
2024-03-24 21:24:45 +0900

    Update Packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="gym_snakegame",
-    version="0.0.1",
-    install_requires=["gymnasium>=0.26.0", "pygame==2.1.0"],
+    name="gym-snakegame",
+    version="0.1.0",
+    author="helpingstar",
+    author_email="iamhelpingstar@gmail.com",
+    description="A gymnasium-based RL environment for learning the snake game.",
+    packages=find_packages(),
+    license="MIT License",
+    python_requires=">=3.9",
+    install_requires=[
+        "numpy>=1.21.0",
+        "pygame>=2.1.3",
+        "gymnasium>=1.0.0a1",
+        "moviepy>=1.0.0",
+    ],
 )
