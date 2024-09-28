3b3c8f298715683a5df1fa34db3d787273412d2c
helpingstar <iamhelpingstar@gmail.com>
2024-09-28 09:34:33 +0900

    Update pyproject.toml

--- gym_snakegame/__init__.py
+++ gym_snakegame/__init__.py
@@ -1,5 +1,7 @@
 from gymnasium.envs.registration import register
 
+__version__ = "0.0.1"
+
 register(
      id="gym_snakegame/SnakeGame-v0",
      entry_point="gym_snakegame.envs:SnakeGameEnv",
--- setup.py
+++ setup.py
@@ -1,18 +1,4 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name="gym-snakegame",
-    version="0.1.0",
-    author="helpingstar",
-    author_email="iamhelpingstar@gmail.com",
-    description="A gymnasium-based RL environment for learning the snake game.",
-    packages=find_packages(),
-    license="MIT License",
-    python_requires=">=3.9",
-    install_requires=[
-        "numpy>=1.21.0",
-        "pygame>=2.1.3",
-        "gymnasium>=1.0.0a1",
-        "moviepy>=1.0.0",
-    ],
-)
+if __name__ == "__main__":
+    setuptools.setup()
